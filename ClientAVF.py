77c682862446e15d9046b21addd9f430205c08d6
DEATHbyEggnog <144825948+DEATHbyEggnog@users.noreply.github.com>
2023-12-14 15:37:18 -0500

    Update ClientAVF.py
    FINAL READY TO MARK

--- ClientAVF.py
+++ ClientAVF.py
@@ -1,3 +1,34 @@
 #Ashley Ferguson 100615979
 #Client Portion of Final Project for TPRG 2131
 #December 6, 2023
+import socket
+import json
+import time
+
+s = socket.socket()
+host = '10.102.13.55'
+port = 5000
+s.connect((host, port))
+
+# Send data 50 times with a delay of 2 seconds
+for _ in range(50):
+    data_dict = {
+        "Temperature": "some_value",  # Replace with actual data
+        "DisplayPower": "some_value",
+        "MeasureClock": "some_value",
+        "MeasureVolts": "some_value",
+        "DiskUsage": "some_value",  # Replace with actual data
+
+    }
+
+    # Convert the dictionary to a JSON-formatted string
+    json_string = json.dumps(data_dict)
+
+    # Send the JSON string as bytes
+    s.send(json_string.encode('utf-8'))
+
+    # Wait for 2 seconds before sending the next update
+    time.sleep(2)
+
+# Close the connection
+s.close()
