8f98d9a74f8bfaee428016cd82d33245504722a1
DEATHbyEggnog <144825948+DEATHbyEggnog@users.noreply.github.com>
2023-12-14 14:04:07 -0500

    Update ClientAVF.py

--- ClientAVF.py
+++ ClientAVF.py
@@ -1,29 +1,35 @@
 #Ashley Ferguson 100615979
 #Client Portion of Final Project for TPRG 2131
 #December 6, 2023
+
+#December 14 updated, 2023
 import socket
 import json
 import time
 
 s = socket.socket()
-host = '10.102.13.65'
+host = '10.102.13.55'
 port = 5000
 s.connect((host, port))
 
-while True:
-    data = s.recv(1024).decode()
-    if not data:
-        s.close()
-        break
-    
-    data_dict = json.loads(data)
-    #print data collected
-    print("Core Temperature: {} C".format(data_dict["Temperature"]))
-    print("Display Power: {} ".format(data_dict["DisplayPower"]))
-    print("Clock Frequency: {} Hz" .format(data_dict["MeasureClock"]))
-    print("DRAM Voltage: {} V" .format(data_dict["MeasureVolts"]))
-    print("Firmware Version: {}".format(data_dict["FirmwareVersion"]))
+# Send data 50 times with a delay of 2 seconds
+for _ in range(50):
+    data_dict = {
+        "Temperature": "some_value",  # Replace with actual data
+        "DisplayPower": "some_value",
+        "MeasureClock": "some_value",
+        "MeasureVolts": "some_value",
+        "Storage": "some_value"
+    }
 
+    # Convert the dictionary to a JSON-formatted string
+    json_string = json.dumps(data_dict)
+
+    # Send the JSON string as bytes
+    s.send(json_string.encode('utf-8'))
+
+    # Wait for 2 seconds before sending the next update
+    time.sleep(2)
+
+# Close the connection
 s.close()
-
-time.sleep(5)
